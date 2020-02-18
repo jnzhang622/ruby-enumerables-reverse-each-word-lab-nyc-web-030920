0f9a5ce40515045d19d4c7be3243af28bf578947
John Zhang <jnzhang622@gmail.com>
2020-02-18 06:38:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,5 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
-  result = []
-  current_word_split = []
-  current_word = []
-  index = 0
-  while index < new_sentence.size do
-    index2 = new_sentence[index].size - 1
-    current_word_split = new_sentence[index].split
-    while index2 >= 0 do
-      current_word << current_word_split[index2]
-      index2 -= 1
-    end
-    result << current_word
-    index =+ 1
-  end
-  result
-  pp result
+	new_sentence = new_sentence.each {|num| num.reverse!}
+	new_sentence.join(" ")
 end
